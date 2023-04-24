9c00248d9a1eee67939c4740f2897c8691717bd5
Muhammad Guruh Ajinugroho <guruh.ajinugroho@ackcio.com>
2023-04-24 19:54:30 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+Go-Lang JWT Cookie Authentication with Fiber Framework following this tutorial (with few adjustments considering the timeskip) :
+
+https://www.youtube.com/watch?v=X9WULjvgqTY
---
+++ database/connection.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"go-auth/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(mysql.Open("rroot:password123_@/go_auth"), &gorm.Config{})
+
+	if err != nil {
+		panic("Couldn't connect to DB")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+}
